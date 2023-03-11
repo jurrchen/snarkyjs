283bfa1533c521617a0a04bad6d07d962a7b6469
Jieren Chen <jieren.chen@gmail.com>
2023-03-11 02:31:46 +0000

    sat checkpoint

--- src/proofchain/proofchain.ts
+++ src/proofchain/proofchain.ts
@@ -6,9 +6,59 @@ import {
   verify,
   isReady,
   provablePure,
+  Struct,
+  Circuit,
 } from 'snarkyjs';
 
+import { 
+  DynamicArray
+} from './dynamicArray.js' // we use JS?
+
 await isReady;
+
+class ProofchainStruct extends Struct({
+  messages: DynamicArray(Field, 100)
+}) {}
+
+
+const Proofchain = Experimental.ZkProgram({
+  publicInput: provablePure(ProofchainStruct),
+
+  methods: {
+    init: {
+      privateInputs: [],
+      method(publicInput: ProofchainStruct) {
+        publicInput.messages.length
+        // assert commitment
+        // publicInput.a.assertEquals(Field(0))
+        // publicInput.b.assertEquals(Field(0))
+      },
+    },
+
+    // addUser
+
+    addMessage: {
+      privateInputs: [],
+
+      method(rollup: ProofchainStruct, previous: Messages, next: Messages, earlierProof) {
+
+
+
+      }
+    }
+  }
+});
+
+
+
+// https://github.com/o1-labs/snarkyjs/blob/main/src/examples/zkapps/merkle_tree/merkle_zkapp.ts#L78
+// https://github.com/Raunaque97/RepeatingLifeZK/blob/main/src/gameOfLife.ts
+
+
+
+
+
+
 
 type CompositeInput = {
   a: Field,
@@ -20,7 +70,7 @@ const CompositeInput = provablePure(
   { customObjectKeys: ['a', 'b'] }
 );
 
-let Program1 = Experimental.ZkProgram({
+const Program1 = Experimental.ZkProgram({
   publicInput: CompositeInput,
 
   methods: {
@@ -38,7 +88,7 @@ let Program1 = Experimental.ZkProgram({
 
 const Program1Proof = Experimental.ZkProgram.Proof(Program1);
 
-let Program2 = Experimental.ZkProgram({
+const Program2 = Experimental.ZkProgram({
   publicInput: Field,
 
   methods: {
@@ -55,29 +105,30 @@ let Program2 = Experimental.ZkProgram({
 
       // and can we add a self proof???
       method(publicInput: Field, anotherProof: Proof<CompositeInput>) {
-        anotherProof.verify();
-        anotherProof.publicInput.a.add(1).assertEquals(publicInput);
-        anotherProof.publicInput.b.add(1).assertEquals(publicInput);
+        // anotherProof.verify();
+        // anotherProof.publicInput.a.add(1).assertEquals(publicInput);
+        // anotherProof.publicInput.b.add(1).assertEquals(publicInput);
       },
     },
   },
 });
 
-console.time('compiling MyProgram...');
+console.time('compiling 1');
 await Program1.compile();
-console.timeEnd('compiling MyProgram...');
+console.timeEnd('compiling 1');
 
 
-console.time('compiling Jank...');
+console.time('compiling 2');
 const { verificationKey } = await Program2.compile();
-console.timeEnd('compiling Jank...');
+console.timeEnd('compiling 2');
 console.log('verification key', verificationKey.slice(0, 10) + '..');
 
-console.log('proving base case...');
+console.time('proving base case...');
 const proof = await Program1.baseCase({
   a: Field(0),
   b: Field(0)
 });
+console.timeEnd('proving base case...');
 
 console.time('proving step 1...');
 const proof2 = await Program2.inductiveCase(Field(1), proof);
